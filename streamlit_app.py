41ee48ddf15fe534a60ad869495281a157c4e8fb
atiullahsaif <36361656+atiullahsaif@users.noreply.github.com>
2023-11-23 11:41:41 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,69 +1,31 @@
 import streamlit as st
 import geopandas as gpd
-import rasterio
-from rasterio import features
-import numpy as np
 import matplotlib.pyplot as plt
-from io import BytesIO
 
 # Streamlit app setup
-st.title('Weighted Overlay Analysis')
+st.title('Weighted Overlay Analysis for CITYAM')
 
-# File uploader for raster layers
-restriction_file = st.file_uploader("Upload 'restriction' shapefile:", type=['shp'])
-parking_file = st.file_uploader("Upload 'parking' shapefile:", type=['shp'])
+# Path to the shapefiles
+restriction_path = r'C:\Users\saifa2\Downloads\Helsinki Geodata\restriction.shp'
+parking_path = r'C:\Users\saifa2\Downloads\Helsinki Geodata\parking.shp'
 
-# Check if files are uploaded
-if restriction_file and parking_file:
-    # Read shapefiles
-    restriction_data = gpd.read_file(restriction_file)
-    parking_data = gpd.read_file(parking_file)
+# Load shapefiles
+restriction = gpd.read_file(restriction_path)
+parking = gpd.read_file(parking_path)
 
-    # Convert shapefiles to raster (assuming a specific raster resolution)
-    # Replace 'transform' and 'shape' with appropriate values based on your data
-    transform = rasterio.transform.from_bounds(*restriction_data.total_bounds, width=500, height=500)
-    shape = (500, 500)
+# User-defined weights
+weight_restriction = 2
+weight_parking = 3
+
+# Perform weighted overlay analysis (simple example)
+if st.button('Perform Overlay'):
+    # Perform overlay operation (e.g., intersection, union, etc.)
+    overlay_result = restriction.intersection(parking)
     
-    # Rasterize the shapefiles to create raster arrays
-    restriction_raster = features.rasterize(
-        [(geom, 1) for geom in restriction_data.geometry],
-        out_shape=shape,
-        transform=transform,
-        fill=0,
-        dtype='float64'
-    )
-    
-    parking_raster = features.rasterize(
-        [(geom, 1) for geom in parking_data.geometry],
-        out_shape=shape,
-        transform=transform,
-        fill=0,
-        dtype='float64'
-    )
-
     # Apply weights
-    restriction_weight = 2
-    parking_weight = 3
-
-    # Perform weighted overlay calculation
-    weighted_overlay = (restriction_raster * restriction_weight) + (parking_raster * parking_weight)
-
+    overlay_result['weighted_score'] = overlay_result.geometry.area * (weight_restriction + weight_parking)
+    
     # Display the resulting overlay map
-    plt.imshow(weighted_overlay, cmap='viridis')
-    plt.title('Weighted Overlay Result')
-    plt.colorbar(label='Overlay Value')
-    plt.axis('off')
-
-    # Show the plot in Streamlit
-    st.pyplot(plt)
-
-    # Option to download the resulting overlay map
-    def download_overlay():
-        buffer = BytesIO()
-        plt.savefig(buffer, format='png')
-        buffer.seek(0)
-        return buffer
-
-    st.markdown("### Download Result")
-    buffer = download_overlay()
-    st.download_button(label="Download Overlay Map", data=buffer, file_name='weighted_overlay.png', mime='image/png')
+    fig, ax = plt.subplots()
+    overlay_result.plot(ax=ax, column='weighted_score', legend=True)
+    st.pyplot(fig)
