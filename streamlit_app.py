94cb2401c28167e82aa16e5055057c34c1512859
atiullahsaif <36361656+atiullahsaif@users.noreply.github.com>
2023-11-23 11:32:23 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,46 +1,29 @@
 import streamlit as st
-import pandas as pd
+import rasterio
+import numpy as np
 
-def multi_criteria_analysis(criteria_weights, alternatives):
-    # Normalize criteria weights
-    total_weight = sum(criteria_weights.values())
-    normalized_weights = {key: value / total_weight for key, value in criteria_weights.items()}
+# Streamlit app setup
+st.title('Weighted Overlay Analysis')
 
-    # Calculate scores for alternatives
-    scores = {}
-    for alt in alternatives:
-        alt_score = sum([normalized_weights[criteria] * alternatives[alt][criteria] for criteria in criteria_weights])
-        scores[alt] = alt_score
+# File uploader for raster layers
+uploaded_files = st.file_uploader("Upload your raster layers:", accept_multiple_files=True, type=['tif'])
 
-    return scores
+# User input for weights
+weights = []
+for file in uploaded_files:
+    weight = st.slider(f"Set weight for {file.name}:", 0.0, 1.0)
+    weights.append(weight)
 
-def main():
-    st.title("Weighted overlay analysis - CITYAM ")
+# Perform weighted overlay analysis (simplified example using numpy)
+if st.button('Perform Overlay'):
+    # Read raster data
+    raster_data = [rasterio.open(file) for file in uploaded_files]
     
-    # Define criteria and alternatives
-    criteria = ['Population density', 'Wind speed', 'Noise level']
-    alternatives = {
-        'Option 1': {'Population density': 5, 'Wind speed': 8, 'Noise level': 7},
-        'Option 2': {'Population density': 7, 'Wind speed': 6, 'Noise level': 9},
-        'Option 3': {'Population density': 4, 'Wind speed': 9, 'Noise level': 6}
-    }
+    # Read and process raster data with weights
+    weighted_arrays = [r.read(1) * weight for r, weight in zip(raster_data, weights)]
     
-    # Collect user input for criteria weights
-    st.sidebar.header('Criteria Weights')
-    criteria_weights = {}
-    for crit in criteria:
-        weight = st.sidebar.slider(f"Weight for {crit}", 0, 10, 5)
-        criteria_weights[crit] = weight
+    # Calculate the weighted sum
+    result_array = np.sum(weighted_arrays, axis=0)
     
-    st.write("Selected Criteria Weights:", criteria_weights)
-    
-    # Perform multi-criteria analysis
-    scores = multi_criteria_analysis(criteria_weights, alternatives)
-    
-    # Display results
-    st.subheader("Scores for Alternatives")
-    df = pd.DataFrame.from_dict(scores, orient='index', columns=['Score'])
-    st.write(df)
-
-if __name__ == "__main__":
-    main()
+    # Display the resulting raster map (simplified example)
+    st.image(result_array, caption='Weighted Overlay Result', use_column_width=True)
